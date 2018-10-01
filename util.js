20f36a464ed51287e7afe27200dfcf128e9aee2c
Victor Hugo do Nascimento <43215136+Victor-H-Nascimento@users.noreply.github.com>
2018-10-01 13:30:53 -0300

    servidor

---
+++ util.js
@@ -0,0 +1,19 @@
+exports.dataTime = function(){
+
+ return Date();
+    
+};
+
+
+exports.createUser = function(){
+    var x = 10, y=5;
+    return x+y;
+
+};
+
+
+exports.test = function(){
+
+    return 'Teste1';
+
+};
