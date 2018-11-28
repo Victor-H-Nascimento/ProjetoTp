dbc53f60144302bd6f786b1a844092bbd6c0bbfc
Victor Hugo do Nascimento <43215136+Victor-H-Nascimento@users.noreply.github.com>
2018-11-28 21:47:10 -0200

    correcoes em acesso

--- ProjetoTp-master/public/js/acesso.js
+++ ProjetoTp-master/public/js/acesso.js
@@ -311,9 +311,14 @@ function alterarDadosPessoais(id) {
             if (dados.status === 'SEMACESSO')
                 alert('Erro: 2 ' + dados.data);
             else {
-                console.log(dados.data[0]);
-                var d = new Date(dados.data[0].dataNascimento);
-                console.log(d);
+                
+                
+                 var date = new Date(dados.data[0].dataNascimento);
+                
+               
+                var years = date.getFullYear();
+                var months = date.getMonth() + 1;
+                var days = date.getDate();
 
                 var dadosPessoais =
                 '<form id="formDadosPessoais" name="formDadosPessoais" action="#" method="post">' +
@@ -328,7 +333,7 @@ function alterarDadosPessoais(id) {
                 '</div>' +
 
                 '<div class="col-md-6 mb-3">' +
-                '<input type="date" name="dataNascimento" class="form-control" id="dataNascimento" value="'+dados.data[0].dataNascimento+'" placeholder="Data de Nascimento" >' +
+                '<input type="date" name="dataNascimento" class="form-control" id="dataNascimento" value="'+years +"-"  +months +"-" +days +'" placeholder="Data de Nascimento" >' +
                 '</div>' +
 
                 '<div class="col-md-6 mb-3">' +
@@ -538,7 +543,7 @@ function exibirDados() {
                 '</div>' +
 
                 '<div class="col-md-6 mb-3">' +
-                '<input type="text" name="rua" class="form-control" id="rua" value="" placeholder="Rua: ' + dados.data[0].rua + '" disabled>' +
+                '<input type="text" name="rua" class="form-control" id="rua" value="" placeholder="' + dados.data[0].rua + '" disabled>' +
                 '</div>' +
 
                 '<div class="col-md-6 mb-3">' +
